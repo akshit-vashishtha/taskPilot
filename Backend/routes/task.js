ecc0bc33d2e00eaedf63b478d8b1e7f9afb7f2c9
akshit-vashishtha <akshitv322@gmail.com>
2025-12-14 11:32:54 +0530

    new project creation part is done

---
+++ Backend/routes/task.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/task');
+
+/**
+ * Get all tasks
+ */
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Create a task
+ */
+router.post('/', async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
